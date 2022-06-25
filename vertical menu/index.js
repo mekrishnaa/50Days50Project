f450ece05edda65ca6f42ba128c8f34252b3e0bb
krishna kumar <krishnakumar356808@gmail.com>
2022-06-25 17:11:18 +0530

    Vertical menu project

---
+++ vertical menu/index.js
@@ -0,0 +1,14 @@
+let list = document.querySelectorAll('li');
+for(let i=0;i<list.length;i++){
+    list[i].onmouseover = function () {
+        let j=0;
+        while(j<list.length){
+            list[j++].className='list';
+
+        }
+        list[i].className='list active';
+        let bg = document.querySelector('body');
+        let colour = list[i].getAttribute('data-color');
+        bg.style.backgroundColor = colour;
+    }
+}
